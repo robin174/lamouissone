d06df82160cb17f8f5184025d3c7616b85e8cfa9
Robin Green <robin@designingreen.co.uk>
2020-03-30 17:28:13 +0100

    Pricing table cleanup

--- templates/block-table--pricing-fr.php
+++ templates/block-table--pricing-fr.php
@@ -9,20 +9,6 @@
 					</tr>
 				</thead>
 				<tbody>
-					<!--
-						<tr>
-						    <td><strong>Printemps 2018</strong><br>01 January - 16 juin*</td>
-						    <td>2,600&euro; par semaine,<br> 370&euro; par nuitée</td>
-						</tr>
-						<tr>
-						    <td><strong>Eté 2018</strong><br>17 juin - 30 août*</td>
-						    <td>3,200&euro; par semaine,<br> 460&euro; par nuitée</td>
-						</tr>
-						<tr>
-						    <td><strong>Hiver 2018</strong><br>01 septembre - 31 décembre</td>
-						    <td>2,600&euro; par semaine,<br> 370&euro; par nuitée</td>
-						</tr>
-					-->
 					<tr>
 					    <td><strong>Printemps 2020</strong><br>01 janvier - 14 juin*</td>
 					    <td>2,600&euro; par semaine,<br> 370&euro; par nuitée</td>
