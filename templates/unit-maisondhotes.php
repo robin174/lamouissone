9a23c6efa80fc38a767df6f702787e9882d021ed
Robin Green <robin@designingreen.co.uk>
2020-03-30 16:45:21 +0100

    Updated url to French version of guesthouse website

--- templates/unit-maisondhotes.php
+++ templates/unit-maisondhotes.php
@@ -9,7 +9,7 @@
 			<h4 style="margin-top:10px;">La maison d'hôtes</h4>
 			<p>Nous disposons d'un espace web dédié dans lequel sont précisés nos tarifs, les disponibilités et caractéristiques de la maison d'hôtes.</p>
 			<p>Pour découvrir notre offre, rendez vous sur notre site Web:<br>
-			<i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://www.lamouissone-maisondhotes.com" target="_blank" title="La maison d'hôtes">www.lamouissone-maisondhotes.com</a></p>
+			<i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://lamouissone-maisondhotes.com/fr/accueil/" target="_blank" title="La maison d'hôtes">www.lamouissone-maisondhotes.com</a></p>
 		</div>
 	</div><!-- end .row -->
 </div>
