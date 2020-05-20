b0a84366ca92b14b8012ae34d1cf0ec5b59c3d57
Robin Green <robin@designingreen.co.uk>
2020-05-20 12:12:43 +0100

    Basic 404 in place

--- 404.php
+++ 404.php
@@ -1,23 +1,22 @@
 <?php get_header(); ?>
-			
-	<div id="content">
-		<div id="inner-content" class="row">
-	
-			<main id="main" class="large-8 medium-8 columns" role="main">
-				<article id="content-not-found">
-				
-					<header class="article-header">
-						<h1><?php _e( '404 - Page Not Found', 'jointswp' ); ?></h1>
-					</header> <!-- end article header -->
-			
-					<section class="entry-content">
-						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
-					</section> <!-- end article section -->
 
-				</article>
-			</main> <!-- end #main -->
+<section class="area--head" style="background:url(<?php the_field('unit_header_image'); ?>)repeat-x top center fixed;">
+	<!-- to update css if no title - adjust padding -->
+	<h1 class="font--overlay-head"><?php the_title(); ?></h1>
+</section>
 
-		</div> <!-- end .row -->
-	</div> <!-- end #content -->
+<section class="area--template">
+	<div class="row">
+		<div class="small-12 medium-4 medium-centered large-4 columns">
+			<!-- picture of hedge trimmers on floor with leaves -->
+		</div>
+		<div class="small-12 medium-8 medium-centered large-8 columns">
+			<section class="block--template">
+				<h1><?php _e( '404 - Page Not Found', 'jointswp' ); ?></h1>
+				<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
+			</section>
+		</div>
+	</div><!-- end .row -->
+</section>
 
 <?php get_footer(); ?>
