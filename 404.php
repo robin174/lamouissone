02460bd59364d6d1ee2ccf4e0357c48fb196605b
Robin Green <robin@designingreen.co.uk>
2020-05-20 12:30:32 +0100

    Basic 404 in place

--- 404.php
+++ 404.php
@@ -7,10 +7,10 @@
 
 <section class="area--template">
 	<div class="row">
-		<div class="small-12 medium-4 medium-centered large-4 columns">
+		<div class="small-12 medium-4 large-4 columns">
 			<!-- picture of hedge trimmers on floor with leaves -->
 		</div>
-		<div class="small-12 medium-8 medium-centered large-8 columns">
+		<div class="small-12 medium-8 large-8 columns">
 			<section class="block--template">
 				<h1><?php _e( '404 - Page Not Found', 'jointswp' ); ?></h1>
 				<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
