64e53a4f435fd94e380dd5510471774fc38ba7a2
Robin Green <robin@designingreen.co.uk>
2020-07-01 11:48:18 +0100

    Minor nav edits

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,13 +1,16 @@
 <div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
+	<!-- .top-bar-left removed -->
+	<div class="float-left">
 		<ul class="menu">
 			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
 		</ul>
 	</div>
-	<div class="top-bar-right show-for-medium">
+	<!-- .top-bar-right removed -->
+	<div class="float-right show-for-large">
 		<?php joints_top_nav(); ?>	
 	</div>
-	<div class="top-bar-right float-right show-for-small-only">
+	<!-- .top-bar-right removed -->
+	<div class="float-right hide-for-large" style="float:right;">
 		<ul class="menu">
 			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
 			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
